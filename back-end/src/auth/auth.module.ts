0bbf9ed65a4de0fe540a9a62c0da75db2e3173b4
Starfosse <77751009+Starfosse@users.noreply.github.com>
2024-12-30 22:21:18 +0100

    fix api

--- back-end/src/auth/auth.module.ts
+++ back-end/src/auth/auth.module.ts
@@ -5,10 +5,17 @@ import { AuthRepository } from './auth.repository';
 import { AuthService } from './auth.service';
 import { GoogleStrategy } from './strategies/google.strategy';
 import { UserRepository } from 'src/user/user.repository';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   imports: [PassportModule],
   controllers: [AuthController],
-  providers: [GoogleStrategy, AuthService, AuthRepository, UserRepository],
+  providers: [
+    GoogleStrategy,
+    AuthService,
+    AuthRepository,
+    UserRepository,
+    UserService,
+  ],
 })
 export class AuthModule {}

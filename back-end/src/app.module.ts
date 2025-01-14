64407f0b4c9eab5ff4905368ab2f14a13f3d0a44
Starfosse <alexis.bouleau92@yahoo.fr>
2025-01-14 23:04:26 +0100

    init

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -3,6 +3,7 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule,
     UserModule,
     DatabaseModule,
+    ProductsModule,
   ],
 })
 export class AppModule {}

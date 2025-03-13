6ed05683d2cc6bdcbab64e1874b3b683703568a2
Starfosse <alexis.bouleau92@yahoo.fr>
2025-03-13 16:06:12 +0100

    Update main.ts

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformResponseInterceptor } from './interceptors/transform.interceptor';
 import * as cookieParser from 'cookie-parser';
-
 async function bootstrap() {
   const isProd = process.env.NODE_ENV === 'production';
   const app = await NestFactory.create(AppModule);
@@ -10,7 +9,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.enableCors({
     origin: isProd
-      ? ['https://front-prod', 'https://accounts.google.com']
+      ? ['https://asgard-forge.vercel.app', 'https://accounts.google.com']
       : ['http://localhost:5173', 'https://accounts.google.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
